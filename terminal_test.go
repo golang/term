f5beecf764ed751c785fa21705385df593b1852d
Tobias Klauser <tklauser@distanz.ch>
2021-04-21 15:23:43 +0200

    term: skip TestMakeRawState on GOOS=ios
    Change-Id: I0068c03d571672db8f809e9308e438e7b030e075
    Reviewed-on: https://go-review.googlesource.com/c/term/+/312250
    Trust: Tobias Klauser <tobias.klauser@gmail.com>
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- terminal_test.go
+++ terminal_test.go
@@ -407,7 +407,7 @@ func TestMakeRawState(t *testing.T) {
 		t.Fatalf("failed to get terminal state from GetState: %s", err)
 	}
 
-	if runtime.GOOS == "darwin" && (runtime.GOARCH == "arm" || runtime.GOARCH == "arm64") {
+	if runtime.GOOS == "ios" || (runtime.GOOS == "darwin" && (runtime.GOARCH == "arm" || runtime.GOARCH == "arm64")) {
 		t.Skip("MakeRaw not allowed on iOS; skipping test")
 	}
 
